a901661c7c4d50c4ce9c3ef96bca9d5af0c3bdc2
PedrodelaFuente <124264586+PedrodelaFuente@users.noreply.github.com>
2025-12-03 18:05:42 +0100

    Dropout

--- Practica2/Parte1/task.py
+++ Practica2/Parte1/task.py
@@ -134,10 +134,14 @@ class MLP(nn.Module):
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, NUM_CLASSES)
 
+        self.dropout = nn.Dropout(0.5)
+
     def forward(self, x):
         x = self.flatten(x)
         x = torch.relu(self.fc1(x))
+        x = self.dropout(x)
         x = torch.relu(self.fc2(x))
+        x = self.dropout(x)
         x = self.fc3(x)
         return x
 
@@ -157,6 +161,7 @@ class SmallCNN(nn.Module):
             nn.Flatten(),
             nn.Linear(64*7*7, 128),
             nn.ReLU(),
+            nn.Dropout(0.5),  
             nn.Linear(128, NUM_CLASSES),
         )
 
